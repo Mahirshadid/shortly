2ae63bb49e5990c0c4e11a8a65d5fa1b63c6dec7
Mahir Shadid <mahir.shadid@gmail.com>
2023-09-28 17:43:04 +0600

    day-4

--- src/components/api-adv/Apiadv.jsx
+++ src/components/api-adv/Apiadv.jsx
@@ -1,16 +1,92 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+
 import br from '../../assets/icon-brand-recognition.svg';
 import dr from '../../assets/icon-detailed-records.svg';
 import fc from '../../assets/icon-fully-customizable.svg';
 import './apiadv.css';
 
-const Apiadv = () => {
-    return (
+// https://api.shrtco.de/v2/shorten?url=
+
+const getLocal = () => {
+    let shortLinks = localStorage.getItem('shortLinks');
+    
+        if (shortLinks) {
+        return JSON.parse(localStorage.getItem('shortLinks'));
+        } else {
+        return [];
+        }
+    };
+    
+    const Apiadv = () => {
+        const [text, setText] = useState('');
+        const [shortLinks, setShortLinks] = useState(getLocal());
+        const [error, setError] = useState('');
+    
+        const handleSubmit = async (e) => {
+        e.preventDefault();
+    
+        if (text.trim() === '') {
+            setError('Empty');
+            return;
+        }
+    
+        const res = await fetch(`https://api.shrtco.de/v2/shorten?url=${text}`);
+        const data = await res.json();
+    
+        if (data.ok) {
+            setError('');
+            const newShortLinks = [data.result.full_short_link, ...shortLinks.slice(0, 4)];
+            setShortLinks(newShortLinks);
+            setText('');
+        } else {
+            setError('Invalid URL');
+        }
+        };
+    
+        const handleCopy = (link) => {
+        navigator.clipboard.writeText(link);
+        };
+    
+        const handleClearAll = () => {
+            setShortLinks([]);
+        };
+
+        useEffect(() => {
+        localStorage.setItem('shortLinks', JSON.stringify(shortLinks));
+        }, [shortLinks]);
+    
+        return (
         <div className="apiadv section__padding">
-            <div className='apiadv-api'>
-                <textarea id="apiadv-text" placeholder='Shorten a link here...' required />
-                <button type="button" id='apiadv-b'>Shorten It!</button>
+            <div className="apiadv-api">
+            <form className="form" onSubmit={handleSubmit}>
+                <input
+                type="text"
+                placeholder="Shorten a link here..."
+                required
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+                />
+                <button type="submit" id="apiadv-b">
+                Shorten It!
+                </button>
+            </form>
+            {error && <div className="error">{error}</div>}
+            <div className="form_api_bar">
+                {shortLinks.map((link, index) => (
+                <div key={index} className="form_api_bar_item">
+                    <button className="link_btn">{link}</button>
+                    <button className="copy_btn" onClick={() => handleCopy(link)}>Copy</button>
+                </div>
+                ))}
+                {shortLinks.length > 0 && ( // Conditionally render the "Clear All" button
+                <button className="clear_btn" onClick={handleClearAll}>
+                Clear All
+                </button>
+            )}
             </div>
+        </div>
+
 
             <div className='apiadv-text'>
                 <h2>Advanced Statistics</h2>
